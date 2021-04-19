43f2db9595858d7a46eb962eeed32b8197dc9fda
Pablo <pablo.zapata.dev@gmail.com>
2021-04-19 23:24:04 +0200

    Ok

---
+++ addons/xpLeveler/server/main.lua
@@ -0,0 +1,12 @@
+---@author Pablo Z.
+---@version 1.0
+--[[
+  This file is part of Astra RolePlay.
+  
+  File [main] created at [19/04/2021 23:23]
+
+  Copyright (c) Astra RolePlay - All Rights Reserved
+
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
