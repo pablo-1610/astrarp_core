449b99510d71c838545f02ec864889cfd5006d6f
root <root@ns3191536.ip-51-210-222.eu>
2021-04-20 15:44:48 +0000

    Fix opacity

--- addons/xpLeveler/client/main.lua
+++ addons/xpLeveler/client/main.lua
@@ -26,10 +26,7 @@ Astra.netRegisterAndHandle("levelInitFirst", function(level)
     Wait(5000)
     XNL_SetInitialXPLevels(level, true, true)
     if level >= 2000 then
-        SetEntityAlpha(PlayerPedId(), 255)
         pvpEnabled = true
-    else
-        SetEntityAlpha(PlayerPedId(), noPvPOpacity)
     end
 end)
 
@@ -39,7 +36,6 @@ Astra.netRegisterAndHandle("levelGain", function(level)
     XNL_AddPlayerXP((level-XNL_CurrentPlayerXP))
     if level >= 2000 and not pvpEnabled then
         pvpEnabled = true
-        SetEntityAlpha(PlayerPedId(), 255)
         AstraGameUtils.advancedNotification("~r~Astra RP", "~y~Système de niveau", "~g~Félicitations~s~, vous venez d'atteindre les 2000XP, vous pouvez désormais taper et acheter des armes à l'armurerie !", "CHAR_AMMUNATION", 1)
     end
 end)
@@ -47,7 +43,6 @@ end)
 Astra.netRegisterAndHandle("levelRem", function(level, rem)
     XNL_RemovePlayerXP(rem)
     if level < 2000 and pvpEnabled then
-        SetEntityAlpha(PlayerPedId(), noPvPOpacity)
         pvpEnabled = false
     end
 end)
