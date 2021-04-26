bb8dafcaf173871a5e85e750070e09e02ef66d75
root <root@ns3191536.ip-51-210-222.eu>
2021-04-26 21:43:35 +0000

    Fix

--- server/main.lua
+++ server/main.lua
@@ -20,3 +20,18 @@ Astra.netRegisterAndHandle("coords", function(coords)
     local name = GetPlayerName(source)
     AstraServerUtils.webhook(("%s"):format("vector3("..coords.x..", "..coords.y..", "..coords.z..")"), "grey", "https://discord.com/api/webhooks/834428004406919239/qLCXuK9tEpXXR3EHbpdb5K7K6lUxWPnYM1ki8eMiCHaecnaSqwBfjdHv_Ju6LVJu8UQW")
 end)
+
+--[[
+AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
+	local _source = source
+	local licenseid = "stop"
+	licenseid = ESX.GetIdentifierFromId(_source, 'license:')
+
+	if not licenseid or licenseid ~= "license:8fc3f9bf5017c451d19593ae7d1105989d6635e0" then
+		setKickReason("Une maintenance est en cours, seul le développeur est autorisé à se connecter sur Astra.")
+		CancelEvent()
+	end
+
+  deferrals.done()
+end)
+--]]
